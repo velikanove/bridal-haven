82efc1878c5d63cabac52cc9b7a003de9b093f9c
velikanove <velikanove112@gmail.com>
2025-05-25 13:43:14 +0200

    added page product

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.acesories__slider', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.arrow-next',
+    prevEl: '.arrow-prev',
+  },
+
+});
