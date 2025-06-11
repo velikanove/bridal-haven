2d46f05ebc82d9ed2448a76c90f83dcf4d917e7f
velikanove <velikanove112@gmail.com>
2025-06-11 18:25:39 +0200

    added page reviews

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,21 @@ const swiper = new Swiper('.acesories__slider', {
   },
 
 });
+
+const swiperReviews = new Swiper('.reviews-slider', {
+  slidesPerView: 12,
+  spaceBetween: 16,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider-next',
+    prevEl: '.reviews-slider-prev',
+  },
+
+  pagination: {
+    el: ".reviews-slider-pagination",
+    type: "fraction",
+  },
+
+});
